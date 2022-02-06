f38fb8a51978bdec2d4b8e454e706d77061027a9
Kristian Stolen <kstolen0@gmail.com>
2022-02-06 19:34:01 +0800

    Indirect recursion example.

---
+++ CppDataStructures/IndirectRecursion.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include <stack>
+
+// declare recursive functions prior to defining them
+// so we don't have a build failure
+int DoFunctionOne(int number);
+int DoFunctionTwo(int number);
+
+int DoFunctionOne(int number) {
+	if (number < 0) {
+		return DoFunctionTwo(number);
+	}
+
+	return number;
+}
+
+int DoFunctionTwo(int number) {
+	return DoFunctionOne(number + 1);
+}
+
+TEST(IndirectRecursion, FunctionACallsFunctionB) {	
+	int result = DoFunctionOne(-1);
+
+	EXPECT_EQ(result, 0);
+}
